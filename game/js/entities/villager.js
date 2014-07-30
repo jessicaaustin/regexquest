b79c925c0c26f2a449ae81ea0bf945ad49f3dabf
jessicaaustin <jessica@aus10.org>
2014-07-30 15:31:10 -0400

    Move zombie villager into its own file

---
+++ game/js/entities/villager.js
@@ -0,0 +1,115 @@
+
+/**
+ * ZOMBIE VILLAGER
+ */
+ game.EnemyEntity = me.ObjectEntity.extend({
+     init: function(x, y, settings) {
+
+        // sprite params
+        settings.image = "zombie";
+        settings.spritewidth = 32;
+        settings.spriteheight= 36;
+
+        // save the area size defined in Tiled
+        var width = settings.width;
+        var height = settings.height;
+
+        // call the parent constructor
+        this.parent(x, y , settings);
+
+        // Set up the sprite animations
+        this.renderable.addAnimation("right", [3, 4, 5]);
+        this.renderable.addAnimation("left", [9, 10, 11]);
+
+         // set start/end position based on the initial area size
+         x = this.pos.x;
+         this.startX = x;
+         this.endX   = x + width - settings.spritewidth
+         this.pos.x  = x + width - settings.spritewidth;
+
+         // walking speed
+         this.setVelocity(1, 0);
+
+         // make it collidable
+         this.collidable = true;
+         this.type = me.game.ENEMY_OBJECT;
+
+         // puzzle setup
+         this.inPuzzle = false;
+     },
+
+     onCollision : function (res, obj) {
+        me.audio.play("zombie2");
+        this.startPuzzle(obj);
+     },
+
+    startPuzzle: function(player) {
+
+        this.inPuzzle = true;
+        this.collidable = false; // prevent collision loop
+
+        player.startPuzzle();
+
+        game.puzzlegui.setupPuzzle(player, this);
+    },
+
+    endPuzzle: function() {
+        this.inPuzzle = false;
+
+        // flicker and make un-collidable for a while to
+        // allow the player to escape if need be
+        var flickerTime = 2000;
+        this.renderable.flicker(flickerTime)
+        var thisObj = this;
+        setTimeout(function() {
+            thisObj.collidable = true;
+        }, flickerTime);
+    },
+
+    onPuzzleSuccess: function() {
+        // TODO turn into villager, play happy sfx, no longer an enemy
+        this.endPuzzle();
+    },
+
+    onPuzzleFail: function() {
+        // Do nothing until the player succeeds or runs away
+    },
+
+    onPuzzleEscape: function() {
+        this.endPuzzle();
+    },
+
+     update: function(dt) {
+         if (!this.inViewport || this.inPuzzle) {
+            // prevent movement
+            return false;
+         }
+
+         // update position
+         if (this.alive) {
+             if (this.walkLeft && this.pos.x <= this.startX) {
+                 this.walkLeft = false;
+                this.renderable.setCurrentAnimation("right");
+             } else if (!this.walkLeft && this.pos.x >= this.endX) {
+                 this.walkLeft = true;
+                this.renderable.setCurrentAnimation("left");
+             }
+             // make it walk
+             this.vel.x += (this.walkLeft) ? -this.accel.x * me.timer.tick : this.accel.x * me.timer.tick;
+
+         } else {
+             this.vel.x = 0;
+         }
+
+         // check and update movement
+         this.updateMovement();
+
+         // update animation if necessary
+         if (this.vel.x!=0 || this.vel.y!=0) {
+             // update object animation
+             this.parent(dt);
+             return true;
+         }
+         return false;
+     }
+ });
