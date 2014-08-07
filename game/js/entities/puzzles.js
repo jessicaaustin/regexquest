4aa19ed4dd813cfeb09a2024cc8938bbb7272d95
jessicaaustin <jessica@aus10.org>
2014-08-07 18:42:59 -0400

    Another puzzle

--- game/js/entities/puzzles.js
+++ game/js/entities/puzzles.js
@@ -10,13 +10,21 @@ game.puzzles = {
         // TODO add corresponding hints for the early puzzles
 
         // Single character find
-        new rq.Zombie("kHkekklpk mkkkekk, Ikkk've beekkkn infkkkekctkked!kk", /k/g),
+        new rq.Zombie("kHkekklpk mkkkekk, Ikkk've beekkkn infkkkekctkked!kk",
+                      /k/g),
 
         // Single character find, case insensitive
-        new rq.Zombie("GgWGhaGtg hgaGppGegngGged tGogG mGeg..g. GIg vigsgited DGrggg. gM. gyegGstergGday aGnGd ngowgg eGveryGogne logogksG sgo deGgliGgcGiogus.G..", /g/gi),
+        new rq.Zombie("GgWGhaGtg hgaGppGegngGged tGogG mGeg..g. GIg vigsgited DGrggg. gM. gyegGstergGday aGnGd ngowgg eGveryGogne logogksG sgo deGgliGgcGiogus.G..",
+                      /g/gi),
 
         // Find with metacharacters
-        new rq.Zombie("Y23o2u12 4the34re! 4St44op!43 I w7an4t y73ou434r4 b6r34ains! .5.. W7a87it 76a m57i768nute, w7h6y 983am I6 s987ayi3ng t3h0i8s?", /\d/g )
+        new rq.Zombie("Y23o2u12 4the34re! 4St44op!43 I w7an4t y73ou434r4 b6r34ains! .5.. W7a87it 76a m57i768nute, w7h6y 983am I6 s987ayi3ng t3h0i8s?",
+                      /\d/g ),
+
+        // "one of": [ ... ] or (...|...|...)
+        new rq.Zombie("jHaqzve youx seezqxn xDzr.z Mxq. axrounzd? He hqzasn't exqen in hiqs laz siqnce thxe xaccident qa qqqfew dayqs qxago.x",
+                      /[zxq]/g )
+
     ),
 
     getCurrent: function() {
