43163a01395da0292476a652eddd98f13a78b6bd
jessicaaustin <jessica@aus10.org>
2014-08-10 08:57:28 -0400

    Tie puzzles to specific zombies

--- game/js/entities/puzzlegui.js
+++ game/js/entities/puzzlegui.js
@@ -70,7 +70,6 @@ game.PuzzleGUI = Object.extend({
             $(".infected").fadeOut();
             puzzleBoxElem.delay( 2500 ).fadeOut( 250 );
 
-            game.puzzles.puzzleSolved();
             this.playerEntity.onPuzzleSuccess();
             this.zombieEntity.onPuzzleSuccess();
 
@@ -122,10 +121,8 @@ game.PuzzleGUI = Object.extend({
     },
 
     setupPuzzle: function(playerEntity, zombieEntity) {
-        this.puzzle = game.puzzles.getCurrent();
-        if (!this.puzzle) {
-            return;
-        }
+        this.puzzle = game.puzzles.getByZombie(zombieEntity.GUID);
+
         $("#zombieText").html(this.puzzle.zombieText());
 
         this.playerEntity = playerEntity;
--- game/js/entities/puzzles.js
+++ game/js/entities/puzzles.js
@@ -1,3 +1,4 @@
+
 /**
  * Array of zombie puzzles to choose from.
  */
@@ -5,23 +6,32 @@ game.puzzles = {
 
     currentRound: 0,
 
+    // this ties particular puzzles to particular zombies the player finds on the map
+    // that way, if they run away and then come back to a particular zombie, it will have the same puzzle
+    zombiesToPuzzles: {},
+
     puzzles: [
 
         new Array().concat(
+        // Roughly following the progression on this page: http://www.zytrax.com/tech/web/regex.htm
 
-            // 1: Single character find
+            // 1: Literal match, single character
+            //      Help me, I've been infected!
             new rq.Zombie("kHkekklpk mkkkekk, Ikkk've beekkkn infkkkekctkked!kk",
-                          /k/g),
+                          /k/g ),
 
-            // 2: Single character find, case insensitive
+            // 2: Single character find, case insensitive ( or use [gG] or use (g|G) )
+            //      Why does everyone look so delicious...
             new rq.Zombie("gWhy Gdoges eGveryGogne logogk sgo deGgliGgcGiogus.G..",
-                          /g/gi),
+                          /g/gi ),
 
             // 3: Find with metacharacters
+            //      You there! Stop! I want your brains! ... Wait a minute, why am I saying this?
             new rq.Zombie("Y23o2u12 4the34re! 4St44op!43 I w7an4t y73ou434r4 b6r34ains! .5.. W7a8it 7a m7i8nute, w7hy 3am I6 s97ayi3ng t3h0is?",
                           /\d/g ),
 
             // 4: "one of": [ ... ] or (...|...|...)
+            //      Yvonne? Is that you? Where am I? What is going on?
             new rq.Zombie("Yqvzxonne? Iqsz thaxt yqou?z qWhxerqe am Iz? Wxhaqt is gxoingz ozqnx?",
                           /[zxq]/g ),
 
@@ -35,19 +45,21 @@ game.puzzles = {
             new rq.Zombie("Todo: Add puzzle 7",
                           /7/g ),
             // 8: TODO: add
+            //      Yvonne is locked up in her house! Now that we are all cured you should go see her!
             new rq.Zombie("Yvonne is locked up in her house! Now that we are all cured you should go see her! (TODO: add puzzle)",
                           /Yvonne/g )
         )
 
     ],
 
-    getCurrent: function() {
-        return game.puzzles.puzzles[game.data.currentLevel][game.puzzles.currentRound];
-    },
-
-    puzzleSolved: function() {
-        game.puzzles.currentRound++;
+    getByZombie: function(zombieGUID) {
+        var puzzle = game.puzzles.zombiesToPuzzles[zombieGUID];
+        if (!puzzle) {
+            puzzle = game.puzzles.puzzles[game.data.currentLevel][game.puzzles.currentRound];
+            game.puzzles.zombiesToPuzzles[zombieGUID] = puzzle;
+            game.puzzles.currentRound++;
+        }
+        return puzzle.zombie;
     }
 
-    // TODO need an equivalent of "puzzle skipped" (should tie a puzzle to a particular villager once it's been triggered)
 };
